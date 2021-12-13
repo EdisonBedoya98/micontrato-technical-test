eab4eea100e1bdbfabaee03e9fabf1be7850cd23
edisonbedoya98 <edison.bedoyag@udea.edu.co>
2021-12-12 20:17:02 -0500

    Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,30 +18,34 @@ const Home = () => {
           Mi nombre es Edison Bedoya García, actualmente trabajo en la
           Universidad de Antioquia dentro de la división de gestión informática,
           ya llevo 2 años en el puesto de desarrollador full stack, actualmente
-          estoy cursando mi ultima materia de la carrera y la idea es graduarme
-          a principios del próximo año. Soy una persona que le apasionan
-          diversas cosas, como por ejemplo el deporte que practico llamado
-          Calistenia, el cual me ha llevado a participar en varias competencias
-          a nivel nacional y he logrado ganar en algunas ocasiones. También me
-          encanta viajar y conocer nuevas culturas y personas, me apasiona mucho
-          mi carrera y por esto intento aprender día a día nuevas cosas que van
-          saliendo, intentando superarme a mí mismo cada día.
+          estoy cursando la ultima materia del pregrado de ingenieria de
+          sistemas y la meta es obtener el titulo a principios del próximo año.
+        </p>
+        <p>
+          Soy una persona que le apasionan diversas cosas, como por ejemplo el
+          deporte que practico llamado Calistenia, el cual me ha llevado a
+          participar en varias competencias a nivel nacional y he logrado ganar
+          en algunas ocasiones. También me encanta viajar, conocer nuevas
+          culturas y personas, me apasiona mucho el mundo de la programación y
+          por esto intento aprender día a día nuevas cosas y estar a la
+          vanguardia, intentando superarme a mí mismo cada día.
         </p>
         <p>
           Son muchos los sueños que tengo a mediano y largo plazo, uno de estos
           es ser Senior React Developer manejando con experticia todo el
           ecosistema del mismo, también me visualizo viajando por todo el mundo,
-          intentando aprender cada día más de todas las experiencias que hay ahí
-          afuera y me encantaria como primeros destinos ir países de habla
-          inglesa, ya que quiero mejorar mi nivel de inglés y veo esto como una
-          gran oportunidad.
+          para aprender más de todas las experiencias que hay ahí afuera y me
+          encantaria como primer destino ir a un país de habla inglesa, ya que
+          quiero mejorar mi nivel de inglés y veo esto como una gran
+          oportunidad.
         </p>
         <p>
           Me considero una persona muy disciplinada que se le mide a cualquier
-          reto que vaya surgiendo en su camino, sin importar lo imposible que
-          pueda parecer, soy empático con las demás personas, por lo general
-          intento generar un agradable ambiente de trabajo con los demás, para
-          así hacer más amena la jornada laboral.
+          reto que vaya surgiendo en el camino, sin importar lo dificil que
+          pueda parecer; ademas de eso, soy empático con las demás personas y me
+          gusta mantener una buena relación de trabajo con los demás, para así
+          hacer más amena la jornada laboral y generar un ambiente de ayuda
+          mutua.
         </p>
       </article>
     </>
